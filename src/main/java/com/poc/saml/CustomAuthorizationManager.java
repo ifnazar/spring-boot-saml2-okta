a5a69e6cf9692ccae6f9bd3739d42c1665d8821f
Igor Fabiano Nazar <nazar@objective.com.br>
2023-04-16 17:18:04 -0300

    Add CustomAuthorizationManager

---
+++ src/main/java/com/poc/saml/CustomAuthorizationManager.java
@@ -0,0 +1,29 @@
+package com.poc.saml;
+
+import java.util.function.Supplier;
+
+import org.springframework.security.authentication.AuthenticationTrustResolver;
+import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.core.Authentication;
+
+public class CustomAuthorizationManager<T> implements AuthorizationManager<T> {
+
+	private final AuthenticationTrustResolver trustResolver = new AuthenticationTrustResolverImpl();
+
+	@Override
+	public AuthorizationDecision check(Supplier<Authentication> authentication, T object) {
+		boolean granted = isGranted(authentication.get());
+		return new AuthorizationDecision(granted);
+	}
+
+	private boolean isGranted(Authentication authentication) {
+		return authentication != null && isNotAnonymous(authentication) && authentication.isAuthenticated();
+	}
+
+	private boolean isNotAnonymous(Authentication authentication) {
+		return !this.trustResolver.isAnonymous(authentication);
+	}
+
+}
